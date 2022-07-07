ff7df4654595270aa3a5875274bbe476c2a012cc
MaxDavidchuk <91936652+MaxDavidchuk@users.noreply.github.com>
2022-07-07 18:01:45 +0300

    Add files via upload

--- Lesson 08/Homework08_task2.py
+++ Lesson 08/Homework08_task2.py
@@ -6,27 +6,56 @@
 import json
 
 def read_data(file_name: str) -> dict:
-	with open(file_name, 'r', encoding='utf-8') as file:
+	with open(file_name, 'r', encoding='utf-8-sig') as file:
 		return json.load(file)
 
-
-def search_data(x: dict) -> None:
-	word = input('> Введіть слово для перекладу: ')
-	if word not in x:
-		print('  Такого слова немає у словнику.')
-	else:
-		print(f'  {word} => {x[word]}')
-
-def print_set(s: set) -> None:
+def display_menu(x: list) -> None:
+    print('Виберіть частину Європи:')
     k = 0
-    for fio in s:
+    for part in x:
         k += 1
-        print(f'{k}. {fio}', end=' ')
-    print()
+        print(f'{k} - {part}')
+    print('0 - Завершити роботу\n')
+
+def get_choice() -> int:
+	choice = int(input('> Виберіть потрібний вам варіант: '))
+	return choice
+
+def break_work() -> bool:
+	result = input('> Продовжити (y/n)? - ')
+	return result == 'n'
+
+def search_data(x: dict, s: str) -> None:
+    print(f'{s} має такі держави:')
+
+    #word = input('> Введіть назву держави: ')
+    #print(x)
+    #for k in range(x):
+    #    if x[k][0]:
+    #    print('  Такого слова немає у словнику.')
+    #else:
+	#	print(f'  {word} => {x[word]}')
 
 if __name__ == '__main__':
     d = read_data('base.json')
-
+    part = ["Західна Європа", "Південна Європа", "Північна Європа", "Східна Європа"]
+    while True:
+        display_menu(part)
+        case = get_choice()
+        if case == 1:
+            search_data(d[str(case)], part[case - 1])
+        elif case == 2:
+            pass
+        elif case == 3:
+            pass
+        elif case == 4:
+            pass
+        elif case == 0:
+            break
+        else:
+            print('Ви вибрали неіснуючий пункт меню.')
+        if break_work():
+            break
     print("""
     =====================
     =       Кінець      =
