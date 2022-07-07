44657c145d70359bfd1444207f3e087c5afb1dcc
MaxDavidchuk <dp_maxim@outlook.com>
2022-07-07 19:17:12 +0300

    2022-07-07

--- Lesson 08/Homework08_task2.py
+++ Lesson 08/Homework08_task2.py
@@ -5,36 +5,42 @@
 
 import json
 
+
 def read_data(file_name: str) -> dict:
-	with open(file_name, 'r', encoding='utf-8-sig') as file:
-		return json.load(file)
+    with open(file_name, 'r', encoding='utf-8-sig') as file:
+        return json.load(file)
+
 
 def display_menu(x: list) -> None:
     print('Виберіть частину Європи:')
     k = 0
-    for part in x:
+    for eur in x:
         k += 1
-        print(f'{k} - {part}')
+        print(f'{k} - {eur}')
     print('0 - Завершити роботу\n')
 
+
 def get_choice() -> int:
-	choice = int(input('> Виберіть потрібний вам варіант: '))
-	return choice
+    choice = int(input('> Виберіть потрібний вам варіант: '))
+    return choice
+
 
 def break_work() -> bool:
-	result = input('> Продовжити (y/n)? - ')
-	return result == 'n'
+    result = input('> Продовжити (y/n)? - ')
+    return result == 'n'
+
 
 def search_data(x: dict, s: str) -> None:
     print(f'{s} має такі держави:')
 
-    #word = input('> Введіть назву держави: ')
-    #print(x)
-    #for k in range(x):
-    #    if x[k][0]:
-    #    print('  Такого слова немає у словнику.')
-    #else:
-	#	print(f'  {word} => {x[word]}')
+
+# word = input('> Введіть назву держави: ')
+# print(x)
+# for k in range(x):
+#    if x[k][0]:
+#    print('  Такого слова немає у словнику.')
+# else:
+#   print(f'  {word} => {x[word]}')
 
 if __name__ == '__main__':
     d = read_data('base.json')
