f500f9542199c9d31a714638c9bad311c666a522
MaxDavidchuk <91936652+MaxDavidchuk@users.noreply.github.com>
2022-07-14 15:30:35 +0300

    Add files via upload

--- Lesson 09/HomeWork/data_manager.py
+++ Lesson 09/HomeWork/data_manager.py
@@ -11,11 +11,16 @@ def load_data(file_name: str) -> dict:
 
 def display_menu() -> None:
     print('\nВиберіть потрібну задачу:')
-    print('\t1 - Перелік департаментів')
-    print('\t2 - Список працівників у конкретному департаменті')
-    print('\t3 - Пошук співробітника за назвою підрозділу та його ім\'ям')
-    print('\t4 - Пошук співробітника лише за його ім\'ям')
-    print('\t0 - Завершити роботу\n')
+    print('\t1 -> Перелік відділів')
+    print('\t2 -> Список працівників у конкретному відділі')
+    print('\t3 -> Пошук співробітника за назвою відділу та його ім\'ям')
+    print('\t4 -> Пошук співробітника лише за його ім\'ям')
+    print('\t5 -> Створити новий відділ')
+    print('\t6 -> Створити (додати) нового співробітника')
+    print('\t7 -> Видалити існуючого співробітника')
+    print('\t8 -> Переведення співробітника між відділами')
+    print('\t9 -> Збільшення зарплати співробітника')
+    print('\t0 -> Завершити роботу\n')
 
 
 def get_choice() -> int:
@@ -28,10 +33,10 @@ def break_work() -> bool:
 	return result == 'n'
 
 
-def save_data(data: dict, file_name: str) -> None:
+def save_data(data: dict) -> None:
 	"""Збереження даних"""
-	with open(file_name, "w", encoding='utf-8') as file:
-		dump(data, file)
+	with open('data.json', "w", encoding='utf-8') as file:
+		dump(data, file, indent=4, ensure_ascii=False)
 	print('Дані успішно збережені!\n')
 
 
@@ -65,7 +70,7 @@ def display_employees(data: dict, dep_name: str) -> None:
 		print('----------------------')
 
 
-# HomeTask-1
+# 2022-07-11 HomeTask-1
 def display_emp_info(data: dict, dep_name: str, emp_name: str) -> None:
 	""" Вивести інформацію про даного співробітника даного підрозділу """
 	print('\n----------------------')
@@ -89,22 +94,103 @@ def display_emp_info(data: dict, dep_name: str, emp_name: str) -> None:
 		print('----------------------\n')
 
 
-# HomeTask-2
-def find_employee(data: dict, emp_name: str) -> None:
+# 2022-07-11 HomeTask-2
+def find_emp(data: dict, emp_name: str, print_yn: bool) -> bool:
 	""" Знайти співробітника із завданим ім'ям """
-	k = 1
-	print('\n----------------------')
-	print(f'\tПошук співробітника <{emp_name}>')
-	print('----------------------')
 	success = False
+	k = 1
 	for dep in data.get('departments'):
 		dep_name = dep.get('dep_name')
 		for emp in dep.get('employees'):
-			e_name = emp.get('emp_name').split()
-			if e_name[1] == emp_name:
-				print(f'{k}. -> {emp.get("emp_name")}\t-> {emp.get("emp_pos")}\t-> {dep_name}')
-				k += 1
+			if emp.get('emp_name').find(emp_name) > -1:
 				success = True
-	if not success:
-		print(f'Співробітник <{emp_name}> - не знайдений!')
-		print('----------------------')
+				if print_yn:
+					print(f'{k}. -> {emp.get("emp_name")}\t-> {emp.get("emp_pos")}\t-> {dep_name}')
+				k += 1
+	return False if not success else True
+
+
+def create_new_dep(data: dict, dep_name: str) -> None:
+    """ Створення нового департаменту """
+    new_dep = {'dep_name': dep_name, 'employees': []}
+    data.get('departments').append(new_dep)
+    save_data(data)
+
+
+def find_dep(data: dict, dep_name: str) -> dict:
+    """ Пошук чи існує підрозділ """
+    for dep in data.get('departments'):
+        if dep.get('dep_name') == dep_name:
+            return dep
+    return {}
+
+
+def create_new_emp(data: dict, dep_name: str, emp_name: str, emp_pos: str, emp_age: int, emp_salary: float) -> None:
+	""" Створення нового співпрбітника """
+	new_emp = {"emp_name": emp_name, "emp_pos" : emp_pos, "emp_age" : emp_age, "emp_salary": emp_salary}
+	dep = find_dep(data, dep_name)
+	if len(dep.items()) > 0:
+		dep.get('employees').append(new_emp)
+	save_data(data)
+
+
+def remove_emp(data: dict, dep_name: str, emp_name: str) -> None:
+	""" Видалити співробітника """
+	dep = find_dep(data, dep_name)
+	if len(dep.get('employees')) > 0:   # Чи існує підрозділ
+		index = -1
+		for emp in dep.get('employees'):    # Пошук індекса
+			index += 1
+			if emp.get('emp_name') == emp_name:
+				break
+		if index > -1:
+			dep.get('employees').pop(index)
+			save_data(data)
+
+
+# 2022-07-13 HomeTask-1
+def transfer_emp(data: dict) -> None:
+    while True:
+        emp_name = input('> Введіть ПІБ співробітника: ')
+        if find_emp(data, emp_name, False):
+            break
+        else:
+            print(f'Співробітник <{emp_name}> не знайдений.')
+    while True:
+        dep_name = input('> Введіть назву Cтарого підрозділу: ')
+        dep_old = find_dep(data, dep_name)
+        if len(dep_old.items()) > 0:
+            break
+        else:
+            print(f'Підрозділ <{dep_name}> не знайдений.')
+    while True:
+        dep_name = input('> Введіть назву Нового підрозділу: ')
+        dep_new = find_dep(data, dep_name)
+        if len(dep_new.items()) > 0:
+            break
+        else:
+            print(f'Підрозділ <{dep_name}> не знайдений.')
+    for emp in dep_old.get('employees'):
+        if emp.get('emp_name') == emp_name:
+            remove_emp(data, dep_old.get('dep_name'), emp_name)
+            dep_new.get('employees').append(emp)
+            print(f'Співробітник <{emp_name}> переміщений у підрозділ <{dep_name}>')
+            save_data(data)
+            break
+
+
+# 2022-07-13 HomeTask-2
+def salary_change(data: dict, dep_name: str, emp_name: str, emp_salary: float) -> None:
+    dep = find_dep(data, dep_name)
+    if len(dep.items()) > 0:
+        for emp in dep.get('employees'):
+            if emp.get('emp_name') == emp_name:
+                salary = float(emp.get('emp_salary'))
+                if salary < emp_salary:
+                    emp.update({"emp_salary": emp_salary})
+                    print(f'Зарплату для співробітника {emp_name} збільшено до {emp_salary}.')
+                    save_data(data)
+                else:
+                    print(f'Існуюча зарплата {salary} більша за запропоновану.')
+                    print('Зарплату не змінено.')
+
--- Lesson 09/HomeWork/main.py
+++ Lesson 09/HomeWork/main.py
@@ -18,7 +18,40 @@ if __name__ == '__main__':
             display_emp_info(data, dep_name, emp_name)
         elif case == 4:
             emp_name = input('> Введіть ім\'я співробітника: ')
-            find_employee(data, emp_name)
+            if find_emp(data, emp_name, True) == False:
+                print(f'Співробітник <{emp_name}> - не знайдений!')
+                print('----------------------')
+        elif case == 5:
+            choice = input('> Бажаєте додати новий підрозділ? (y/n) - ')
+            if choice == 'y':
+                dep_name = input('> Введіть назву нового підрозділу: ')
+                create_new_dep(data, dep_name)
+        elif case == 6:
+            choice = input('> Бажаєте додати нового співробітника? (y/n) - ')
+            if choice == 'y':
+                dep_name = input('> Введіть назву підрозділу: ')
+                emp_name = input('> Введіть повністю ПІБ: ')
+                emp_pos = input('> Введіть бажану посаду: ')
+                emp_age = int(input('> Введіть вік: '))
+                emp_salary = float(input('> Введіть бажану ЗП: '))
+                create_new_emp(data, dep_name, emp_name, emp_pos, emp_age, emp_salary)
+        elif case == 7:
+            choice = input('> Бажаєте видалити співробітника? (y/n) - ')
+            if choice == 'y':
+                dep_name = input('> Введіть назву підрозділу: ')
+                emp_name = input('> Введіть повністю ПІБ: ')
+                remove_emp(data, dep_name, emp_name)
+        elif case == 8:
+            choice = input('> Бажаєте перевести співробітника в інший відділ? (y/n) - ')
+            if choice == 'y':
+                transfer_emp(data)
+        elif case == 9:
+            choice = input('> Бажаєте змінити зарплату співробітнику? (y/n) - ')
+            if choice == 'y':
+                dep_name = input('> Введіть назву підрозділу: ')
+                emp_name = input('> Введіть ПІБ співробітника: ')
+                emp_salary = float(input('> Введіть бажану ЗП: '))
+                salary_change(data, dep_name, emp_name, emp_salary)
         elif case == 0:
             break
         else:
